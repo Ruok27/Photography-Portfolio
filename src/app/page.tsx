466625bbe264613e8ef7b691bd7dbc443f3a8b65
Ruok27 <jakxii27@gmail.com>
2023-12-18 23:41:33 -0500

    Add own images and lightbox galley

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,39 @@ import Image from 'next/image'
 import Link from 'next/link'
 import Masonry from 'react-masonry-css'
 import ClassNames from "classnames"
+
+import LightGalleryComponent from 'lightgallery/react';
+
+// import styles
+import 'lightgallery/css/lightgallery.css';
+import 'lightgallery/css/lg-zoom.css';
+import 'lightgallery/css/lg-thumbnail.css';
+
+// import plugins if you need
+import lgThumbnail from 'lightgallery/plugins/thumbnail';
+import lgZoom from 'lightgallery/plugins/zoom';
+
 import ocean1 from "../app/ocean1.jpg"
 import ocean2 from "../app/ocean2.jpg"
 import ocean3 from "../app/ocean3.jpg"
 import ocean4 from "../app/ocean4.jpg"
 import ocean5 from "../app/ocean5.jpg"
+import ocean6 from "../app/ocean6.jpg"
+
+import type { LightGallery } from 'lightgallery/lightgallery';
+//import { LightGallery } from 'lightgallery/lightgallery';
+
 
 import bgImage from "../app/cameraBG.jpg"
+import { useRef } from 'react';
 
 
 
 // Use custom fonts a custom background image and custom logo 
 // To publish changes to vercel app just go to left tab write message then ctrl enter. input password twice
+
+
+
 const tabs = [
 
   {
@@ -41,26 +62,31 @@ const tabs = [
 
 
 const images = [
-  ocean1, ocean2, ocean3, ocean4, ocean5
+  ocean1, ocean2, ocean3, ocean4, ocean5, ocean6
 
 ]
 
 
 export default function Home() {
+
+  const lightboxRef = useRef<LightGallery | null>(null)
+
+
+
   return (
 
     <div className="h-full overflow-auto">
 
       <div className='fixed left-0 top-0 w-full h-full z-10 from-stone-900 bg-gradient-to-t'></div>
 
-<Image
-                      className='fixed left-0 top-0'
-                      src={bgImage}
-                      priority={true}
-                      alt="background-image"
-                      placeholder='blur'
+      <Image
+        className='fixed left-0 top-0'
+        src={bgImage}
+        priority={true}
+        alt="background-image"
+        placeholder='blur'
 
-                    />
+      />
 
 
       <header className='fixed bg-stone-900 top-0 w-full z-30 flex justify-between items-center h-[90px] px-10'>
@@ -122,28 +148,57 @@ export default function Home() {
             <Tab.Panels className="h-full  max-w-[900px] w-full p-2 sm:p-4 my-6">
               <Tab.Panel>
                 <Masonry breakpointCols={2} className='flex gap-4' columnClassName=''>
-
-                  {images.map((image) => (
+                  {/* Add background color here on image so image doesnt mesh with pictures */}
+                  {images.map((image, idx) => (
                     <Image
                       key={image.src}
                       src={image}
                       alt="placeholder"
-                      className='my-4'
+                      className='my-4 hover:opacity-70 cursor-pointer'
                       placeholder='blur'
+                      onClick={() => {
+
+                        lightboxRef.current?.openGallery(idx)
+
+                      }}
                     />
                   ))}
-
-                  <Image src={ocean1} alt="picture" className='my-4' />
-                  <Image src={ocean2} alt="picture" className='my-4' />
-                  <Image src={ocean3} alt="picture" className='my-4' />
-                  <Image src={ocean4} alt="picture" className='my-4' />
-                  <Image src={ocean5} alt="picture" className='my-4' />
-
 
 
                 </Masonry>
 
+                <LightGalleryComponent
+                  onInit={(ref) => {
 
+                    if (ref) {
+
+
+                      lightboxRef.current = ref.instance
+
+
+                    }
+
+
+
+                  }}
+
+                  speed={500}
+                  plugins={[lgThumbnail, lgZoom]}
+                  dynamic
+                  dynamicEl={images.map(image => ({
+
+
+                    src: image.src,
+                    thumb: image.src,
+
+
+
+                  }))}            >
+
+
+
+
+                </LightGalleryComponent>
 
               </Tab.Panel>
               <Tab.Panel>Oceans</Tab.Panel>
@@ -153,11 +208,11 @@ export default function Home() {
         </div>
 
 
-      </main>
+      </main >
       <footer className='relative h-[90px] z-20 flex justify-center items-center uppercase text-lg font-medium'>
         <p>Jose Fernandez | Photography Portfolio</p>
       </footer>
 
-    </div>
+    </div >
   )
 }
